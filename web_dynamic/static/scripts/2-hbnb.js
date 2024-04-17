bebcbfc13dd2c9b32ebca9fe7c84540f60a3e4fe
isiji <blairisiji@gmail.com>
2024-04-17 14:16:55 +0300

    2hbnb

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,7 +1,31 @@
-$.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
-  if (textStatus === 'success') {
-    $('div#api_status').addClass('available');
+function init() {
+
+  const amenityObj = {};
+  $('amenities .popover input').change(function () {
+    if ($(this).is(':checked')) {
+      amenityObj[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenityObj[$(this).attr('data-id')];
+    }
+    if (Object.values(amenityObj).length > 0) {
+      $('div.amenities h4').text(Object.values(amenityObj).join(', '));
+    } else {
+      $('div.amenities h4').html('&nbsp;');
+    }
+  });
+
+  apiStatus();
+}
+
+function apiStatus() {
+
+  const url = 'http://$(HOST):5001/api/v1/status/';
+
+  $.get(url, function (data, status) {
+    if (status === 'success') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
   }
-}).fail(function () {
-  $('div#api_status').removeClass('available');
-});
+}
